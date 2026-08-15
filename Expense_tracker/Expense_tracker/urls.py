a013b1cc5e57922e063aaea34e4121a4344bb755
AjayGupta-8828 <ajaysonugupta5698@gmail.com>
2026-08-15 14:28:10 +0530

    feat: configure django-allauth with Google OAuth login

--- Expense_tracker/Expense_tracker/urls.py
+++ Expense_tracker/Expense_tracker/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from expenses.views import *
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,6 +23,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("",mainpage,name="mainpage"),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path("register/",register_user,name="register"),
     path("login/",login_user,name="login"),
