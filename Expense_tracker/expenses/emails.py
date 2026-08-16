0cbe45ff110c8563ec496942f800996fbcb6f090
AjayGupta-8828 <ajaysonugupta5698@gmail.com>
2026-08-16 13:18:25 +0530

    fix: use settings.DEFAULT_FROM_EMAIL instead of hardcoded sender address

--- Expense_tracker/expenses/emails.py
+++ Expense_tracker/expenses/emails.py
@@ -1,6 +1,8 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+from Expense_tracker.Expense_tracker import settings
+
 
 def send_welcome_email(user_email, user_name):
     subject = "Welcome to Expense Tracker"
@@ -11,5 +13,18 @@ def send_welcome_email(user_email, user_name):
     })
 
     msg = EmailMultiAlternatives(subject, text_content, 'ajaylegend506@gmail.com', [user_email])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+
+def send_otp_email(user_email, user_name, otp_code):
+    subject = "Your Expense Tracker Verification Code"
+    text_content = f"Your OTP is {otp_code}. It expires in 10 minutes."
+    html_content = render_to_string('emails/otp_email.html', {
+        'user_name': user_name,
+        'otp_code': otp_code,
+    })
+
+    msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [user_email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
